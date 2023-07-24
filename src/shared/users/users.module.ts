0c0d8bd09b0b323d255666cc059e4c8542821d57
Naman Arora <namanarora1022@gmail.com>
2023-07-24 17:03:46 +0530

    made a users endpoint added some mg commands in package.json 🚀

--- src/shared/users/users.module.ts
+++ src/shared/users/users.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { User } from './user.model';
+import { UsersController } from './users.controller';
 
 @Module({
     imports: [MikroOrmModule.forFeature([User])],
     providers: [UsersService],
+    controllers: [UsersController],
 })
 export class UsersModule {}
