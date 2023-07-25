4886cfead2dabe19655ee6d23218f93f3d4c222d
Naman Arora <namanarora1022@gmail.com>
2023-07-25 17:23:07 +0530

    refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { configuration, validate } from './config';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { AuthModule } from './auth/auth.module';
 
 // Modules
 import { UsersModule } from './shared/users/users.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
     imports: [
@@ -17,11 +17,10 @@ import { AuthModule } from './auth/auth.module';
             validate,
         }),
         MikroOrmModule.forRoot(),
+        AuthModule,
 
         // Modules
         UsersModule,
-
-        AuthModule,
     ],
     controllers: [],
     providers: [],
